330bccf176e7381a30d9922a6bc85fe551935aef
yangsongyuan <yangjason2012@gmail.com>
2021-01-13 11:57:52 +0800

    1

--- Final/ShellSort.py
+++ Final/ShellSort.py
@@ -1,7 +1,8 @@
 def  shellSort(numList):
     n = len(numList)
     if n == 0 or n == 1:
-        return numList # 陣列只有0個或1個數值所以不用排序，直接return
+        return numList
+    # 陣列只有0個或1個數值所以不用排序，直接return
     gap = n // 2  # 將數字總個數除以2
     while gap >= 1:
         for i in range(gap,n):
@@ -12,6 +13,7 @@ def  shellSort(numList):
                 tempindex -= gap
                 # 先把一個子序列中的元素排好序，某個子序列中的元素下標之間的個隔為gap
         gap = gap // 2 # 將數字總個數除以2
-    return numList #
+    return numList 
 numlist = [4,5,7,8,6,1,2,3,4]
-print(shellSort(numlist))
+print("原列表為：%s" % numlist)
+print("新列表為：%s" % shellSort(numlist))
